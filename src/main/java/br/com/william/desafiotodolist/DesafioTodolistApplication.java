7c7e5695b107db21222fa5760a1b07e1042ed10a
wkoju84 <wkoju84@gmail.com>
2023-12-04 13:21:12 -0300

    adicionada estrutura de camadas

---
+++ src/main/java/br/com/william/desafiotodolist/DesafioTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.william.desafiotodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTodolistApplication.class, args);
+	}
+
+}
